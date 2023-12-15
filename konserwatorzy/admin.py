0709ba298afb19839057019fc6874a682c1e7941
student <e@e>
2023-12-15 12:04:44 +0100

    models commit

--- konserwatorzy/admin.py
+++ konserwatorzy/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from konserwatorzy.models import Zadanie
+
+admin.site.register(Zadanie)
--- konserwatorzy/models.py
+++ konserwatorzy/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class zadanie(models.Model):
+class Zadanie(models.Model):
     nazwa = models.CharField(max_length=50)
     opis = models.CharField(max_length=2000)
     data_postu = models.DateField()
 
     Stan = models.CharField(choices=(
-        'wykonane',
-        'w trakcie',
-        'oczekujące'
+        (1,'wykonane'),
+        (2,'w trakcie'),
+        (3,'oczekujące')
     ), max_length=1)
 
