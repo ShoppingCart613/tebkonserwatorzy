5c0ac59615cd11f47825bd7059cdb20d0b51b4c0
ErwinŁukasik <57984886+ShoppingCart613@users.noreply.github.com>
2024-01-08 08:45:19 +0100

    finished base

--- konserwatorzy/urls.py
+++ konserwatorzy/urls.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as login
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:zadanie_id>/", views.zadanie, name="zadanie"),
-    path("add_zadanie", views.add_zadanie, name="add_zadanie"),
+    path("add_zadanie/", views.add_zadanie, name="add_zadanie"),
+    path("login/", login.LoginView.as_view(), name="login"),
 ]
--- konserwatorzy/views.py
+++ konserwatorzy/views.py
@@ -2,20 +2,33 @@ from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.http import HttpResponse
 from .models import Zadanie
-from .forms import ZadanieForm
+from .forms import ZadanieForm, LoginForm
+from django.contrib.auth.decorators import login_required
 
+#logowanie
+def form_login(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = LoginForm()
+    return render(request, 'konserwatorzy/login.html', {'form': form})
+
+#zadania
+@login_required
 def index(request):
     zadanie_list = Zadanie.objects.order_by("-data_postu")[:5]
     template = loader.get_template("konserwatorzy/index.html")
     context={"zadanie_list": zadanie_list}
     return HttpResponse(template.render(context,request))
 
+@login_required
 def zadanie(request, zadanie_id):
     zadanie = get_object_or_404(Zadanie, pk=zadanie_id)
     template = loader.get_template("konserwatorzy/zadanie.html")
     context={"zadanie": zadanie}
     return HttpResponse(template.render(context,request))
 
+@login_required
 def add_zadanie(request):
     if request.method == 'POST':
         zadanie_form = ZadanieForm(request.POST)
